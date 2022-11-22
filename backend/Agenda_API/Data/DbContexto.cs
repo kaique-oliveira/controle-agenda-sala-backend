a50182d976948bc7d7f71d96cb0a3de9e381bd0a
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2022-11-21 22:22:14 -0300

    criado o DbContext

---
+++ backend/Agenda_API/Data/DbContexto.cs
@@ -0,0 +1,18 @@
+﻿using Agenda_API.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Agenda_API.Data
+{
+    public class DbContexto : DbContext
+    {
+
+        public DbContexto(DbContextOptions<DbContexto> options)
+            : base(options) 
+        {
+        }
+        
+
+        public DbSet<Setor> Setor { get; set; } 
+        public DbSet<Agendamento> Agendamento { get; set; }
+    }
+}
