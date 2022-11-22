0247dda8f6b6081fe14ef3354df6e6b763037241
kaique-oliveira <kaiqueoliveiraantonio@gmail.com>
2022-11-21 22:21:58 -0300

    criada as entidades Setor e Agendamento

---
+++ backend/Agenda_API/Entidades/Agendamento.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Agenda_API.Entidades
+{
+    public class Agendamento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public TimeOnly HourStart { get; set; }
+
+        [Required]
+        public TimeOnly HourEnd { get; set; }
+
+        [Required]
+        public int IdSetor { get; set; }
+    }
+}
---
+++ backend/Agenda_API/Entidades/Setor.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Agenda_API.Entidades
+{
+    public class Setor
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public bool Type { get; set; }
+
+        //public List<Agendamento> Agendamentos { get; set; }
+    }
+
+
+
+}
